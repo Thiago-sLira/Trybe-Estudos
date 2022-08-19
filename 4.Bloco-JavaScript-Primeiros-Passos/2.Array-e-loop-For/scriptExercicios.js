8bdb7cf09a49d1f574140f24a6c129d4be47f913
Thiago Lira <thiago.slira@outlook.com>
2022-08-19 16:08:01 -0300

    Exercicios arrays e for

---
+++ 4.Bloco-JavaScript-Primeiros-Passos/2.Array-e-loop-For/scriptExercicios.js
@@ -0,0 +1,20 @@
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu.length;
+
+// console.log(menuServices);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portfólio');
+
+// console.log(indexOfPortfolio);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// menu.push('Contato');
+
+// console.log(menu);
+
+let numero = 7;
+
+for(let index = 1; index <= 9; index ++) {
+    console.log(numero*index);
+}
