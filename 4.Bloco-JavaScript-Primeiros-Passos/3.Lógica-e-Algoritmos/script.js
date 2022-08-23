2d50c672d74bcc1d56cb4be3c0baaf8c93433aba
Thiago Lira <thiago.slira@outlook.com>
2022-08-23 13:57:55 -0300

    exercicios 4.3

--- 4.Bloco-JavaScript-Primeiros-Passos/3.Lógica-e-Algoritmos/script.js
+++ 4.Bloco-JavaScript-Primeiros-Passos/3.Lógica-e-Algoritmos/script.js
@@ -3,10 +3,18 @@
 
 // 10! = 10 x 9 x 8 x 7 x 6 x 5 x 4 x 3 x 2 x 1 = '?';
 
-let array = [];
-let resultado = 0;
+// let resultado = 1;
 
-for (index = 10; index < array.length; index -= 1) {
-    resultado = array[index] * resultado;
-}
- console.log(resultado);
+// for (let index = 10; index >= 1; index -= 1) {
+//   resultado *= index;
+//   console.log(resultado);
+// }
+
+// console.log(resultado);
+ 
+let word = 'tryber';
+let wordReverse = '';
+
+for (let index = 0; index < word.length; index += 1) {
+  wordReverse += word[word.length - 1 - index];
+} console.log(wordReverse);
