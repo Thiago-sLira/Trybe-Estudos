5363e96c5eb1f23c4700b1ed97d4ddfc848f77d8
Thiago Lira <thiago.slira@outlook.com>
2022-08-23 18:11:01 -0300

    Finalizado aula ao vivo e práticas, agora exercicios

---
+++ 4.Bloco-JavaScript-Primeiros-Passos/4.Objetos-e-Funções/forInForOf.js
@@ -0,0 +1,30 @@
+// let cars = ['Saab', 'Volvo', 'BMW'];
+
+// for (let key in cars) {
+//   console.log(key, cars[key]);
+// };
+
+// let car = {
+//     type: 'Fiat',
+//     model: '500',
+//     color: 'white',
+//   };
+  
+//   for (let key in car) {
+//     console.log(key, car[key]);
+//   };
+
+//   let food = ['hamburguer', 'bife', 'acarajé'];
+// for (let value of food) {
+//   console.log(value);
+// };
+
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+  };
+
+  for (let key in names) {
+    console.log('Olá', names[key]);
+  }
