151401c91e11a5e8dbd1869d4797efc173ef1227
Thiago Lira <thiago.slira@outlook.com>
2022-08-23 11:44:50 -0300

    Exercicio 7 com duvida

--- 4.Bloco-JavaScript-Primeiros-Passos/2.Array-e-loop-For/exercicios.js
+++ 4.Bloco-JavaScript-Primeiros-Passos/2.Array-e-loop-For/exercicios.js
@@ -48,14 +48,36 @@
 // }
 // console.log(maiorValor); // exercicio 5
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27, 289];
-let valorImpar = 0;
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27, 289];
+// let valorImpar = 0;
 
-for (let index = 0; index < numbers.length; index = index + 1) {
-    if (numbers[index] % 2 === 0) {
-       console.log('nenhum valor ímpar encontrado');
-    } else {
-        console.log(numbers[index]);  
+// for (let index = 0; index < numbers.length; index = index + 1) {
+//     if (numbers[index] % 2 === 0) {
+//        console.log('nenhum valor ímpar encontrado');
+//     } else {
+//         console.log(numbers[index]);  
+//     }
+// } // exercicio 6
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27, 289];
+let menorValor = 100000;
+
+for (index = 0; index < numbers.length; index += 1) {
+    //menorValor = numbers[index]
+    if (numbers[index] < menorValor) {
+        menorValor = numbers[index]
     }
-}
-// console.log(valorImpar); // exercicio 6
+} console.log(menorValor);
+
+// let array = [];
+
+// for (index = 0; index <= 25; index =+ 1) {
+//     array.push(array[index]);
+// } console.log(array);
+
+// let word = 'tryber';
+// let wordInverse = '';
+
+// for (index = 0; index < word.length; index += 1) {
+//     wordInverse += word[word.length -1 - index];
+// } console.log(wordInverse); // exercicio 2 (4.3)
