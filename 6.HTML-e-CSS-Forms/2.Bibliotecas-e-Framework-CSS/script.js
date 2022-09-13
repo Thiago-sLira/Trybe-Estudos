a19a261f53c5e67689e6beb26a87a1c87cebe246
Thiago Lira <thiago.slira@outlook.com>
2022-09-13 15:49:11 -0300

    Exercicios 6.5

---
+++ 6.HTML-e-CSS-Forms/2.Bibliotecas-e-Framework-CSS/script.js
@@ -0,0 +1,9 @@
+const elementOl = document.querySelector('#ol');
+const elementCheckin = document.querySelector('.inputCheckin');
+const elementCheckout = document.querySelector('.inputCheckout');
+const elementBedroom = document.querySelectorAll('.bedroom');
+
+
+function reserva('click', () {
+   
+})
