416dd182147d7d099c59131637dde8fca3ce7a7a
Thiago Lira <thiago.slira@outlook.com>
2022-09-26 18:39:23 -0300

    Exercicios

---
+++ 8.Higher-Order-Functions/3.HOFs-Map-and-Filter/exercicio1.test.js
@@ -0,0 +1,6 @@
+const getName = require('./exercicio1');
+
+test('Deve retornar o nome do livro - gênero do livro - nome da pessoa autora', () => {
+    expect(typeof getName).toBe('function');
+    expect(getName()).toEqual(['As_Crônicas_de_Gelo_e_Fogo - Fantasia - George_R._R._Martin', 'O_Senhor_dos_Anéis - Fantasia - J._R._R._Tolkien']);
+});
